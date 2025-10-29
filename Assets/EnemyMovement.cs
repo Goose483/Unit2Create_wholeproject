23e4a01253d6370b0e668b4dc0a2b33c96152172
Goose483 <goocifer323@gmail.com>
2025-10-29 14:31:18 -0400

    Add EnemyMovement to invader prefabs and fix movement logic
    Added the EnemyMovement script to all Invader prefabs and set their movement parameters. Fixed typos and improved the movement coroutine in EnemyMovement.cs, including a ResetPosition method. Updated invader prefab positions and removed a direct instance from the GameScene, likely to rely on prefab instantiation.

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -1,35 +1,59 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public Vector3 speed;
+    public Vector3 speedOpposite;
+    public Vector3 up;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCorutine(StartMoving());
+        StartCoroutine(StartMoving());
     }
 
     IEnumerator StartMoving()
     {
+
         for (int i = 0; i < 3; i++)
         {
             yield return new WaitForSeconds(0.2f);
-            GetComponent<Transorm>().position += speed;
+            GetComponent<Transform>().position += speed;
         }
-
         GetComponent<Transform>().position += up;
         for (int i = 0; i < 6; i++)
         {
             yield return new WaitForSeconds(0.2f);
             GetComponent<Transform>().position += speedOpposite;
         }
-
         GetComponent<Transform>().position += up;
         for (int i = 0; i < 3; i++)
         {
             yield return new WaitForSeconds(0.2f);
             GetComponent<Transform>().position += speed;
         }
-
         GetComponent<Transform>().position += up;
-        StartCorutine(StartMoving());
+        StartCoroutine(StartMoving());
     }
+
+     /*void OnCollisionEnter2D(Collision2D collision)
+     {
+        if (collision.gameObject.CompareTag("reset"))
+        {
+            Debug.Log("reset: " + collision.gameObject.name);
+        }
+     }*/
+     
+     private Vector3 originalPosition;
+
+        void Awake()
+        {
+            originalPosition = transform.position;
+        }
+
+        public void ResetPosition()
+        {
+            transform.position = originalPosition; // Reset to the stored original position
+        }
 }
